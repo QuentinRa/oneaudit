7ce1ac30929a92f0487f6bac180709636f6aa66f
Quentin RA <quentin.ramsamy.cf@gmail.com>
2024-11-27 19:41:11 +0100

    Adding local module initialisation

---
+++ oneaudit/modules/socosint/linkedin/__init__.py
@@ -0,0 +1,10 @@
+import oneaudit.modules.socosint.linkedin.scrap
+import oneaudit.modules.socosint.linkedin.export
+import oneaudit.modules.socosint.linkedin.parse
+
+def define_args(parent_parser):
+    linkedin_module = parent_parser.add_parser('linkedin')
+    linkedin_module_action = linkedin_module.add_subparsers(dest='action', help="Action to perform.", required=True)
+    oneaudit.modules.socosint.linkedin.scrap.define_args(linkedin_module_action)
+    oneaudit.modules.socosint.linkedin.export.define_args(linkedin_module_action)
+    oneaudit.modules.socosint.linkedin.parse.define_args(linkedin_module_action)
