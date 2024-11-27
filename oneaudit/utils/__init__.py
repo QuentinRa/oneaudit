cff0e27bf08edec9ec962777249c5be204847411
Quentin RA <quentin.ramsamy.cf@gmail.com>
2024-11-27 20:09:15 +0100

    Adding local module initialisation

--- oneaudit/utils/__init__.py
+++ oneaudit/utils/__init__.py
@@ -1,28 +1,44 @@
-import argparse
-import colorlog
+from argparse import ArgumentParser
+from colorlog import ColoredFormatter
+from sys import stdout
 import logging
-import sys
 
-def args_verbose_config(parser: argparse.ArgumentParser):
+_project_logger = None
+
+def args_verbose_config(parser: ArgumentParser):
     verbose = parser.add_mutually_exclusive_group()
     verbose.add_argument('-v', dest='is_debug', action='store_true', help='Debug verbosity level.')
 
+
 def args_parse_parse_verbose(obj, args):
     obj.log_level = logging.DEBUG if args.is_debug else logging.INFO
-    project_logger = logging.getLogger('oneaudit')
-    project_logger.setLevel(obj.log_level)
+    get_project_logger(obj.log_level)
 
-    console_handler = logging.StreamHandler(sys.stdout)
-    formatter = colorlog.ColoredFormatter(
-        '%(log_color)s%(asctime)s - %(levelname)s - %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S',
-        log_colors={
-            'DEBUG': 'blue',
-            'INFO': 'green',
-            'WARNING': 'yellow',
-            'ERROR': 'red',
-            'CRITICAL': 'bold_red',
-        }
-    )
-    console_handler.setFormatter(formatter)
-    project_logger.addHandler(console_handler)
+
+def get_project_logger(log_level=logging.INFO):
+    global _project_logger
+    if _project_logger is None:
+        _project_logger = logging.getLogger('oneaudit')
+        _project_logger.setLevel(log_level)
+
+        console_handler = logging.StreamHandler(stdout)
+        formatter = ColoredFormatter(
+            '%(log_color)s%(asctime)s - %(levelname)s - %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S',
+            log_colors={
+                'DEBUG': 'blue',
+                'INFO': 'green',
+                'WARNING': 'yellow',
+                'ERROR': 'red',
+                'CRITICAL': 'bold_red',
+            }
+        )
+        console_handler.setFormatter(formatter)
+        _project_logger.addHandler(console_handler)
+    else:
+        return _project_logger
+
+
+def args_call_target(objects, args, key, method):
+    target = getattr(args, key)
+    getattr(objects.get(target, None), method)(args)
