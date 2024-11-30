6d7560d019a0e7f2703f3b76d455633e3719b389
Quentin RA <quentin.ramsamy.cf@gmail.com>
2024-11-30 12:09:47 +0100

    Working on censored passwords

--- oneaudit/modules/leaks/clean.py
+++ oneaudit/modules/leaks/clean.py
@@ -150,25 +150,28 @@ def define_args(parent_parser):
     args_verbose_config(clean_leaks)
 
 
-def censor_password(password, keep_start):
+def censor_password(password, mode):
     """
     Input: toto1
-    => to**1 if we censor and keep the start
-    => t**o1 if we censor and keep the end
+    => to**1 if we censor and keep the start (mode=0)
+    => t**o1 if we censor and keep the end (mode=1)
+    => t***1 if we censor and keep neither (mode=2)
     """
     password_length = len(password)
     if password_length <= 1:
         return '*' if password_length == 1 else ''
     if password_length <= 3:
         return password[0] + "*" * (password_length-1)
-    if password_length == 4:
+    if password_length == 4 or mode == 2:
         return password[0] + "*" * (password_length-2) + password[-1]
-    if keep_start:
+    if mode == 0:
         first, second, last = password[0], password[1], password[-1]
         return first + second + '*' * (password_length - 3) + last
-    else:
+    elif mode == 1:
         first, before_last, last = password[0], password[-2], password[-1]
         return first + '*' * (password_length - 3) + before_last + last
+    else:
+        raise ValueError(f"Invalid value for mode: {mode}.")
 
 def run(args):
     # try:
@@ -190,26 +193,34 @@ def run(args):
         if not credential['passwords'] and not credential['censored_passwords']:
             continue
 
-        unknown_censored_passwords = []
         passwords = [p for p in credential['passwords'] if 4 <= len(p) < 25]
         known_censored_passwords = {}
+        unknown_censored_passwords = []
+        new_passwords = passwords
 
-        for know_password in passwords:
-            for trail in possible_trails:
-                password = know_password + trail
-                for aura_mode in [True, False]:
-                    censored = censor_password(password, keep_start=aura_mode)
-                    known_censored_passwords[censored] = password
-                    known_censored_passwords[censored[0].lower() + censored[1:]] = password[0].lower() + password[1:]
-                    known_censored_passwords[censored[0].upper() + censored[1:]] = password[0].upper() + password[1:]
+        while new_passwords:
+            passwords_to_process = new_passwords
+            new_passwords, unknown_censored_passwords = [], []
 
-        for censored_password in credential['censored_passwords']:
-            if censored_password in known_censored_passwords:
-                passwords.append(known_censored_passwords[censored_password])
-                continue
-            if not 4 <= len(censored_password) < 25:
-                continue
-            unknown_censored_passwords.append(censored_password)
+            for know_password in passwords_to_process:
+                for trail in possible_trails:
+                    password = know_password + trail
+                    for censor_mode in range(0, 3):
+                        censored = censor_password(password, censor_mode)
+                        known_censored_passwords[censored] = password
+                        known_censored_passwords[censored[0].lower() + censored[1:]] = password[0].lower() + password[1:]
+                        known_censored_passwords[censored[0].upper() + censored[1:]] = password[0].upper() + password[1:]
+
+            for censored_password in credential['censored_passwords']:
+                if censored_password in known_censored_passwords:
+                    candidate = known_censored_passwords[censored_password]
+                    if candidate not in passwords:
+                        new_passwords.append(candidate)
+                        passwords.append(candidate)
+                    continue
+                if not 4 <= len(censored_password) < 25:
+                    continue
+                unknown_censored_passwords.append(censored_password)
 
         # If we knew of every censored hash, we remove them
         credential['passwords'] = [p for p in set(passwords) if p != "(null)"]
@@ -218,4 +229,8 @@ def run(args):
             for unknown_censored_password in unknown_censored_passwords:
                 logger.warning(f"Uknown censored password {unknown_censored_password} for {credential['passwords']}")
 
-
+    save_to_json(args.output_file, {
+        "version": 1.0,
+        "index": 0,
+        "credentials": credentials
+    })
